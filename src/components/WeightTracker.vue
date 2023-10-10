ab139d03e74e07feecef3492bd2b5e70c8718836
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-10-09 22:42:46 -0500

    Add DaisyUI

--- src/components/WeightTracker.vue
+++ src/components/WeightTracker.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="h-[100vh] bg-black">
     <h1 class="flex justify-center text-3xl font-bold pt-5 text-yellow-500 tracking-wider">Weight Tracker</h1>
+    <button class="btn btn-primary">Hello Daisy button</button>
   </div>
 </template>
