2a32317c02a4973669f5a457a1ddfb42ec29f0a5
Steven Boutcher <boutchersj@gmail.com>
2023-10-20 23:25:16 -0500

    Style the dialog

--- src/components/DialogThing.vue
+++ src/components/DialogThing.vue
@@ -41,15 +41,14 @@
 
 <template>
     <div id='dialog' class="hidden absolute inset-0 z-[100] bg-[#070707]">
-        <div class="flex justify-center items-center h-48 w-96 bg-white">
-            <p :onclick="closeDialog" class="absolute top-2 right-4">X</p>
-            <p>{{ props.message }}</p>
-            <form @submit.prevent="handleSubmit" class="flex flex-col justify-center text-[#4B4B4B] text-lg h-[50%] tracking-wide">
-                <div id="current-weight-group" class="flex flex-col mb-5 mx-3">
-                    <label class="" for="current-weight">Add your current weight</label>
-                    <input v-model="currentWeight" class="placeholder:text-[#BDBDBD] h-14 pl-3 mr-5 rounded-lg bg-white border border-[#BDBDBD]" type="number" id="current-weight" name="current-weight" step="0.01" placeholder="enter current weight" required>
+        <div class="flex justify-center items-center h-54 w-80 bg-white my-[50%] pl-5 rounded-xl">
+            <p :onclick="closeDialog" class="absolute top-2 right-[72px] my-[50%] text-2xl">✕</p>
+            <form @submit.prevent="handleSubmit" class="flex flex-col justify-center items-start text-[#4B4B4B] text-lg w-full tracking-wide">
+                <div id="current-weight-group" class="flex flex-col mb-5 w-full">
+                    <label class="mb-3 mt-6 text-xl font-normal" for="current-weight">{{ props.message }}</label>
+                    <input v-model="currentWeight" class="placeholder:text-[#BDBDBD] h-14 pl-3 mr-10 rounded-lg bg-white border border-[#BDBDBD]" type="number" id="current-weight" name="current-weight" step="0.1" placeholder="enter current weight" required>
                 </div>
-                <input class="mt-auto bg-[#2058E8] py-4 px-5 mx-3 rounded-xl text-white font-semibold" type="submit" value="Add weight" onsubmit="">
+                <input class="mt-auto mb-5 bg-[#2058E8] py-4 w-[90%] rounded-xl text-white font-semibold" type="submit" value="Add weight" onsubmit="">
             </form>
         </div>
     </div>
