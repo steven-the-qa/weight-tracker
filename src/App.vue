73232cd5fbb748142c380a20b92fa235b7db05a1
Steven Boutcher <boutchersj@gmail.com>
2023-10-22 16:20:54 -0500

    Style the signout button

--- src/App.vue
+++ src/App.vue
@@ -35,5 +35,5 @@ const handleSignout = () => {
   </header>
 
   <RouterView class="h-[90%]"/>
-  <button @click="handleSignout" v-if="isLoggedIn"> Sign out</button>
+  <button class="bg-red-500 w-full flex justify-center items-center h-10 text-white bottom-0 absolute" @click="handleSignout" v-if="isLoggedIn"> Sign out</button>
 </template>
