6354494b83d2aad629aff6697f6c14f836084794
Steven Boutcher <boutchersj@gmail.com>
2023-10-31 11:05:38 -0700

    Check URL after the UI is verified

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('Login page is loaded', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveURL(/.*login/)
   await expect(page.getByTestId('header-text')).toHaveText('Track your weight loss journey with ease and simplicity');
+  await expect(page).toHaveURL(/.*login/)
 })
