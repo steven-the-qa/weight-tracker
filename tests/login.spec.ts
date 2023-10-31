ad5e97792078ddfb89e550637856c9d7945e4e7a
Steven Boutcher <boutchersj@gmail.com>
2023-10-31 12:03:33 -0700

    Add more stuff that worked in my other project

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,21 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Browser, BrowserContext, Page, chromium } from '@playwright/test';
 
-test('Login page is loaded', async ({ page }) => {
-  await page.goto('/');
-  // await expect(page.getByTestId('header-text')).toHaveText('Track your weight loss journey with ease and simplicity');
-  await expect(page).toHaveURL(/.*login/)
+let browser: Browser;
+let context: BrowserContext;
+let page: Page;
+
+test.beforeAll(async () => {
+  // For Debugging Only
+  // browser = await chromium.launch({ headless: false });
+  browser = await chromium.launch();
+  context = await browser.newContext();
+  page = await context.newPage();
+})
+
+test.describe('Login', async () => {
+  test('Login page is loaded', async () => {
+    await page.goto('/');
+    await expect(page.getByTestId('header-text')).toHaveText('Track your weight loss journey with ease and simplicity');
+    await expect(page).toHaveURL(/.*login/)
+  })
 })
