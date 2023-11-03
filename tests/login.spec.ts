9043f806d8779cef66774b59f70188f229dc71fa
Steven Boutcher <boutchersj@gmail.com>
2023-11-03 09:35:16 -0500

    Try full URL

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-
 test.afterAll(async ({ browser }) => {
   await browser.close()
 })
 
 test.describe('Login', () => {
   test('Login page is loaded', async ({ page }) => {
-    await page.goto('/');
+    await page.goto('http://localhost:5173/');
     await page.content().then(content => console.log(content))
     await expect(page.getByTestId('header-text')).toHaveText('Track your weight loss journey with ease and simplicity');
   })
