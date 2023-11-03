9be44703328241ebaa4b28ee834728edf8b02442
Steven Boutcher <boutchersj@gmail.com>
2023-11-03 07:55:02 -0500

    Restructure the test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,21 +1,14 @@
-import { test, expect, Browser, BrowserContext, Page, chromium } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-let browser: Browser;
-let context: BrowserContext;
-let page: Page;
 
-test.beforeAll(async () => {
-  // For Debugging Only
-  // browser = await chromium.launch({ headless: false });
-  browser = await chromium.launch();
-  context = await browser.newContext();
-  page = await context.newPage();
+test.afterAll(async ({ browser }) => {
+  await browser.close()
 })
 
-test.describe('Login', async () => {
-  test('Login page is loaded', async () => {
-    await page.goto('http://localhost:5173/');
-    await page.waitForURL('http://localhost:5173/login')
+test.describe('Login', () => {
+  test('Login page is loaded', async ({ page }) => {
+    await page.goto('/');
+    await page.waitForURL('/login')
     await expect(page.getByTestId('header-text')).toHaveText('Track your weight loss journey with ease and simplicity');
   })
 })
