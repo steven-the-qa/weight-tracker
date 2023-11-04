037861f3289aa2aff6761f55117b8b2004febf32
Steven Boutcher <boutchersj@gmail.com>
2023-11-04 08:40:26 -0500

    Try using Prod URL

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,7 +6,7 @@ test.afterAll(async ({ browser }) => {
 
 test.describe('Login', () => {
   test('Login page is loaded', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
+    await page.goto('/');
     await page.content().then(content => console.log(content))
     await expect(page.getByTestId('header-text')).toHaveText('Track your weight loss journey with ease and simplicity');
   })
