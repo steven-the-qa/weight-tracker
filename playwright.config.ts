9cece4f00ea09187d7ce2182dbc5d3a191c6e6f6
Steven Boutcher <boutchersj@gmail.com>
2023-11-04 09:14:49 -0500

    Go back to running the server

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run build',
+    command: 'npm run dev',
     url: 'http://localhost:5173',
     timeout: 30000
   }
