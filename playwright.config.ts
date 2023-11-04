34aefdb83f62832175d25c3beef3bc1850eadaa6
Steven Boutcher <boutchersj@gmail.com>
2023-11-04 09:09:45 -0500

    configure webserver in PW config

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,14 +71,9 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    /**
-     * Use the dev server by default for faster feedback loop.
-     * Use the preview server on CI for more realistic testing.
-    Playwright will re-use the local server if there is already a dev-server running.
-     */
-    command: process.env.CI ? 'vite preview --port 5173' : 'vite dev',
-    port: 5173,
-    reuseExistingServer: !process.env.CI
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    timeout: 30000
   }
 }
 
