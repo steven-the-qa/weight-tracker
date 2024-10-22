59ecfbcb37e42056111c9244f3b69864eb6a9272
Steven Boutcher <boutchersj@gmail.com>
2024-10-22 07:09:59 -0500

    Clean up missed reference change

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
   import Dashboard from '../components/Dashboard.vue';
-  import LoadingThing from '../components/Loading.vue'
+  import Loading from '../components/Loading.vue'
 </script>
 
 <template>
   <main class="h-[90%] w-full bg-white text-black">
     <Suspense>
       <Dashboard />
-      <template #fallback><LoadingThing /></template>
+      <template #fallback><Loading /></template>
     </Suspense>
   </main>
 </template>
