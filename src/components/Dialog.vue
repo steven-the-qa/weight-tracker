22eb25c182fb6114d4c166352902ab9f6022affa
Steven Boutcher <boutchersj@gmail.com>
2024-10-22 04:42:54 -0500

    for the Dialog too

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
     import { doc, serverTimestamp, setDoc, collection, DocumentReference, CollectionReference, addDoc} from "firebase/firestore";
     import { db } from '../firebase'
