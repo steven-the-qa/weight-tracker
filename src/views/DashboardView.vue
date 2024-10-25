b17e0601becb2087838bb87fbfd9eb52130f036f
Steven Boutcher <boutchersj@gmail.com>
2024-10-25 05:25:01 -0500

    Move the Loading UI into a view and remove the Loading component

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
   import Dashboard from '../components/Dashboard.vue';
-  import Loading from '../components/Loading.vue'
+  import LoadingView from './LoadingView.vue'
 </script>
 
 <template>
   <main class="h-[90%] w-full bg-white text-black">
     <Suspense>
       <Dashboard />
-      <template #fallback><Loading /></template>
+      <template #fallback><LoadingView /></template>
     </Suspense>
   </main>
 </template>
--- src/views/LoadingView.vue
+++ src/views/LoadingView.vue
@@ -1,4 +1,3 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div class="h-[90%] flex justify-center items-center font-extrabold tracking-wide text-4xl">
         Loading...
