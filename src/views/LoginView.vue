f41095f56ec197b692fbe59d7b35226478a62154
Steven Boutcher <boutchersj@gmail.com>
2024-10-28 14:42:39 -0500

    Fix mobile-responsiveness issues for Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -37,33 +37,37 @@
 </script>
 
 <template>
-  <main class="bg-white text-black">
-    <div class="flex flex-col w-full min-w-[330px] h-screen">
-    <header class="flex-shrink-0">
-      <h1 data-testid="app-name" class="flex justify-center items-center h-12 bg-[#2E7EFD] text-white text-lg font-bold lg:justify-start lg:pl-10 lg:text-3xl lg:bg-white lg:text-[#2E7EFD] lg:h-16 lg:font-['Kanit'] sm:text-xl">
-        MyWeightTracker
-      </h1>
-    </header>
-    <div class="flex flex-col items-top flex-grow lg:flex-row lg:items-start overflow-hidden">
-      <div class="hidden lg:block lg:w-1/2 bg-[url('/src/assets/yoga.png')] bg-cover bg-center h-full"></div>
-      <section class="w-full lg:w-1/2 flex flex-col h-full justify-between py-4">
-        <div class="flex flex-col justify-between">
-          <p data-testid="header-text" class="px-5 pb-2 bg-white text-black font-medium text-lg xs:text-xl lg:text-2xl">
-            Track your weight loss journey with ease and simplicity
-          </p>
-          <ul data-testid="goal-list" role="list" class="ml-12 list-disc font-light text-sm xs:text-base lg:text-lg">
-            <li>Set a goal</li>
-            <li>Track towards the goal</li>
-          </ul>
-        </div>
-        <div class="flex justify-center w-full">
-          <img class="w-full max-h-[40vh] object-contain sm:w-[65%] md:w-[50%]" src="/src/assets/splash.png" alt="Athletic man facing the user, smiling with a thumbs-up gesture, standing next to an athletic woman with her body facing away but head turned back to look at the user, with blue splash pattern backdrop"/>
-        </div>
-        <div class="flex flex-col items-center w-full">
-          <p class="flex justify-center items-center h-10 text-lg font-bold xs:text-xl">Let's get started</p>
-          <GoogleLogin />
-        </div>
-      </section>
+  <main class="h-screen w-full bg-white text-black overflow-hidden">
+    <div class="flex flex-col w-full h-full">
+      <header class="flex-shrink-0">
+        <h1 data-testid="app-name" class="flex justify-center items-center h-12 bg-[#2E7EFD] text-white text-lg font-bold lg:justify-start lg:pl-10 lg:text-3xl lg:bg-white lg:text-[#2E7EFD] lg:h-16 lg:font-['Kanit'] sm:text-xl">
+          MyWeightTracker
+        </h1>
+      </header>
+      <div class="flex flex-col items-top flex-1 lg:flex-row lg:items-start">
+        <div class="hidden lg:block lg:w-1/2 bg-[url('/src/assets/yoga.png')] bg-cover bg-center h-full"></div>
+        <section class="w-full lg:w-1/2 flex flex-col h-full justify-between p-4">
+          <div class="flex flex-col">
+            <p data-testid="header-text" class="px-5 pb-2 text-black font-medium text-lg xs:text-xl lg:text-2xl">
+              Track your weight loss journey with ease and simplicity
+            </p>
+            <ul data-testid="goal-list" role="list" class="ml-12 list-disc font-light text-sm xs:text-base lg:text-lg">
+              <li>Set a goal</li>
+              <li>Track towards the goal</li>
+            </ul>
+          </div>
+          <div class="flex justify-center w-full flex-1 py-4">
+            <img
+              class="w-full max-h-[35vh] object-contain sm:w-[65%] md:w-[50%]"
+              src="/src/assets/splash.png"
+              alt="Athletic man facing the user, smiling with a thumbs-up gesture, standing next to an athletic woman with her body facing away but head turned back to look at the user, with blue splash pattern backdrop"
+            />
+          </div>
+          <div class="flex flex-col items-center w-full">
+            <p class="flex justify-center items-center h-10 text-lg font-bold xs:text-xl">Let's get started</p>
+            <GoogleLogin />
+          </div>
+        </section>
       </div>
     </div>
   </main>
