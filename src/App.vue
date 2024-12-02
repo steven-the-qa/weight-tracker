b29504a672fcde716f851799bbe20a08845a7c05
Steven Boutcher <boutchersj@gmail.com>
2024-12-01 21:10:14 -0600

    Switch back to 8rem for RouterView height

--- src/App.vue
+++ src/App.vue
@@ -86,5 +86,5 @@ watch(isLoggedIn, async (newValue) => {
     @handleSignout="handleSignout"
   />
 
-  <RouterView class="min-w-[300px] h-[calc(100vh-3rem)] overflow-hidden"/>
+  <RouterView class="min-w-[300px] h-[calc(100vh-8rem)] overflow-hidden"/>
 </template>
